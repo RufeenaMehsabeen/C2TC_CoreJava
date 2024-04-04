c3a066365f401132b33790b0eb6c808b336ef99c
rufee <rufee@172.16.176.221>
2024-04-04 08:32:13 +0530

    First Task

--- C38/src/com/tns/dayone/FirstProgram.java
+++ C38/src/com/tns/dayone/FirstProgram.java
@@ -3,7 +3,9 @@ package com.tns.dayone;
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+			System.out.println("Hello World"); 
+		
 
 	}
 
